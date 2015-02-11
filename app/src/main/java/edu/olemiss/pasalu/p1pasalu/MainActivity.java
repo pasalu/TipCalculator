2f91ddb725653b0c597f84e99247f9c40cf0258f
Peter Salu <psalua@gmail.com>
2015-02-10 21:49:24 -0600

    Added calculation of tip.

--- app/src/main/java/edu/olemiss/pasalu/p1pasalu/MainActivity.java
+++ app/src/main/java/edu/olemiss/pasalu/p1pasalu/MainActivity.java
@@ -12,17 +12,27 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+	private static final int ONE_PERSON = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		setupTipPercent();
+		setupNumberOfPeople();
+	}
+
+
+	private void setupTipPercent() {
 		SeekBar tipPercentSeekBar = (SeekBar) findViewById(R.id.tipPercent);
 		tipPercentSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 			@Override
@@ -43,10 +53,78 @@ public class MainActivity extends ActionBarActivity {
 			}
 		});
 
-
 		CharSequence initialProgress = Integer.toString(tipPercentSeekBar.getProgress()) + "%";
 		TextView tipPercentTextView = (TextView) findViewById(R.id.tipPercentText);
 		tipPercentTextView.setText(initialProgress);
+	}
+
+	private void setupNumberOfPeople() {
+		NumberPicker numberOfPeople = (NumberPicker) findViewById(R.id.numberOfPeople);
+		numberOfPeople.setMinValue(1);
+		numberOfPeople.setMaxValue(99);
+
+		numberOfPeople.setOnScrollListener(new NumberPicker.OnScrollListener() {
+			@Override
+			public void onScrollStateChange(NumberPicker view, int scrollState) {
+				TextView people = (TextView) findViewById(R.id.numberOfPeopleText);
+
+				if(view.getValue() == ONE_PERSON) {
+					people.setText(R.string.person);
+				} else {
+					people.setText(R.string.people);
+				}
+			}
+		});
+	}
+
+	public void onCalculateClick(View button) {
+		EditText billEditText = (EditText) findViewById(R.id.bill);
+
+		try {
+			double bill = Double.parseDouble(billEditText.getText().toString());
+			SeekBar tipPercentSeekBar = (SeekBar) findViewById(R.id.tipPercent);
+			double tipPercent = (double)tipPercentSeekBar.getProgress() / 100;
+			double tip = tipPercent * bill;
+			double total = tip + bill;
+
+			TextView tipAmountTextView = (TextView) findViewById(R.id.tipAmount);
+			TextView totalTextView = (TextView) findViewById(R.id.total);
+			NumberPicker numberOfPeopleNumberPicker =
+					(NumberPicker) findViewById(R.id.numberOfPeople);
+
+			int numberOfPeople = numberOfPeopleNumberPicker.getValue();
+
+			String tipAmountString = getString(R.string.tip_amount);
+			String totalString = getString(R.string.total);
+
+			if(numberOfPeople != ONE_PERSON) {
+				tip /= numberOfPeople;
+				total /= numberOfPeople;
+
+				totalString = getString(R.string.total_per_person);
+			}
+
+			tip = twoDecimalPlaces(tip);
+			total = twoDecimalPlaces(total);
+
+			tipAmountString += " ";
+			totalString += " ";
+
+			tipAmountTextView.setText(tipAmountString + Double.toString(tip));
+			totalTextView.setText(totalString + Double.toString(total));
+		} catch (NumberFormatException numberFormatException) {
+			billEditText.setError(getString(R.string.bill_format_error));
+		}
+	}
+
+	/**
+	 * Rounds a number to two decimal places.
+	 * @param number The number to be rounded.
+	 * @return number rounded to two decimal places.
+	 */
+	//Taken from: http://stackoverflow.com/questions/11701399/round-up-to-2-decimal-places-in-java
+	private double twoDecimalPlaces(double number) {
+		return Math.round(number * 100.0) / 100.0;
 	}
 
 
